f7226519a7b490dd097b99d33c35ce3242abd1f8
cel3ntano <adriano2024celentano@gmail.com>
2025-03-17 23:53:56 +0100

    chore: initialize project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  icons: "/favicon.svg",
+  title: "Questioneer – Build and Analyze Interactive Questionnaires",
+  description: "Create, manage, and analyze engaging questionnaires with ease. Drag-and-drop builder, real-time completion tracking, and insightful analytics all in one place.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
